7d257d77799606e88b678d0c365645444c7cfde3
Sanchit Malhotra <sanchit.malhotra@wynk.in>
2022-02-09 12:05:26 +0530

    implementation

--- template/golang-protobuf/function/handler.go
+++ template/golang-protobuf/function/handler.go
@@ -1,20 +1,143 @@
 package function
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"sort"
+	"strings"
+
+	pb "handler/function/generated_grpc"
 
 	handler "github.com/openfaas/templates-sdk/go-http"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 )
 
-// Handle a function invocation
+type Rerank_request struct {
+	User_id         string     `json:"user_id"`
+	Item_ids        [20]string `json:"item_ids"`
+	Qid             string     `json:"qid"`
+	Normalise_items bool       `json:"normalise_items"`
+	Normalise_user  bool       `json:"normalise_user"`
+	Use_case        string     `json:"use_case"`
+}
+type Rerank_response struct {
+	User_id string             `json:"user_id"`
+	Scores  map[string]float64 `json:"scores"`
+	Qid     string             `json:"qid"`
+	Status  int                `json:"status"`
+}
+type Pair struct {
+	Key   string
+	Value float64
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p PairList) Less(i, j int) bool { return p[j].Value < p[i].Value }
+
 func Handle(req handler.Request) (handler.Response, error) {
-	var err error
+	var request_body map[string]interface{}
+	fmt.Println(string(req.QueryString))
+	json.Unmarshal([]byte(string(req.Body)), &request_body)
+	uid := strings.Split(req.QueryString, "=")[1]
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial("host.docker.internal:56937", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
 
-	message := fmt.Sprintf("Body: %s", string(req.Body))
+	c := pb.NewFunctionServiceClient(conn)
 
-	return handler.Response{
-		Body:       []byte(message),
+	contextRequest := pb.LiveContextRequest{
+		Uid:      uid,
+		ArtistId: "",
+	}
+	request := pb.FunctionRequest{
+		Query: &contextRequest,
+	}
+
+	response, err := c.GetFunctionResponse(context.Background(), &request)
+	if err != nil {
+		log.Fatalf("Error when calling onloop: %s", err)
+	}
+
+	var contents [20]string
+	length := len(response.Contents)
+	if length > 20 {
+		length = 20
+	}
+	for i := 0; i < length; i++ {
+		contents[i] = response.Contents[i].GetContentId()
+	}
+	rerank_request := &Rerank_request{User_id: uid, Item_ids: contents, Qid: "25", Normalise_items: false, Normalise_user: false, Use_case: "song"}
+	postBody, err := json.Marshal(rerank_request)
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+	responseBody := bytes.NewBuffer(postBody)
+	resp, err := http.Post("http://discovery-monitoring.wynk.in/v2/reranking/rerank", "application/json", responseBody)
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+	defer resp.Body.Close()
+	//Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// sb := string(body)
+	// log.Printf(sb)
+	rerank_response := Rerank_response{}
+	if resp.StatusCode == http.StatusOK {
+		json.Unmarshal(body, &rerank_response)
+	}
+	log.Println(rerank_response)
+
+	p := make(PairList, len(rerank_response.Scores))
+
+	i := 0
+	for k, v := range rerank_response.Scores {
+		p[i] = Pair{k, v}
+		i++
+	}
+
+	var sortedSongs [20]string
+	sort.Sort(p)
+	for i := 0; i < len(p); i++ {
+		sortedSongs[i] = p[i].Key
+	}
+	log.Println("-----------------")
+	log.Println(sortedSongs)
+	log.Println(request_body)
+
+	request_body["content"] = sortedSongs
+	log.Println(request_body)
+
+	message, err := json.Marshal(request_body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var testrr map[string]interface{}
+	json.Unmarshal(message, &testrr)
+	log.Println(testrr)
+	total_response := &handler.Response{
+		Body:       message,
 		StatusCode: http.StatusOK,
+		Header:     req.Header,
+	}
+	log.Println(*total_response)
+	return handler.Response{
+		Body:       message,
+		StatusCode: http.StatusOK,
+		// Header:     req.Header,
 	}, err
+
 }
--- template/golang-protobuf/go.mod
+++ template/golang-protobuf/go.mod
@@ -1,7 +1,15 @@
 module handler
 
-go 1.17
+go 1.16
 
 replace handler/function => ./function
 
-require github.com/openfaas/templates-sdk v0.0.0-20200723110415-a699ec277c12
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/openfaas/templates-sdk v0.0.0-20200723110415-a699ec277c12
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd
+	golang.org/x/sys v0.0.0-20220207234003-57398862261d // indirect
+	google.golang.org/genproto v0.0.0-20220207185906-7721543eae58 // indirect
+	google.golang.org/grpc v1.44.0
+	google.golang.org/protobuf v1.27.1 // indirect
+)
